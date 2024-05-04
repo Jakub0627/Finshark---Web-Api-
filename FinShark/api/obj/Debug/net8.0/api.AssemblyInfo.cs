e5cf5f91207de5e00e1bb6fbe95e565321f6462d
Jakub <kuba.gladysz@hotmail.com>
2024-05-04 23:23:41 +0200

    Add POST functionality
    Changes:
    - created CreateStockRequestDto.cs
    - added functionality to Stock Mapper
    - Added POST to StockController.cs

--- FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ee6e56079db11d189b1ee29b8880625b8aec14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed30a76544104c22181e66637a13fd1b0eec9708")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
