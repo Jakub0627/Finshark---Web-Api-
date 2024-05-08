3e7d3a7db341cf4647a855e0c2ac8ba4ac0b07fb
Jakub <kuba.gladysz@hotmail.com>
2024-05-08 19:06:10 +0200

    Create Comment System
    Implementation of similar functionality to comments. Currently Get methods were implemented.

--- FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d58020c99d8b1af1fc1b557f64b88507613bd88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9366e251cd5540a3b174715c60ff9c008f72725")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
